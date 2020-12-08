b7d93bef4c523260587d646324580e3cd9d0cdcc
sandraprv <65662489+sandraprv@users.noreply.github.com>
2020-12-08 09:04:08 +0100

    update js - reset form

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 const form = document.getElementById("form");
-const email = document.getElementById("email");
+var email = document.getElementById("email");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  const emailVal = email.value;
+  var emailVal = email.value;
 
   if (!validateEmail(emailVal)) {
     document.getElementById("email").classList.add("error");
@@ -16,12 +16,11 @@ form.addEventListener("submit", (event) => {
     document.getElementById("inputMsg").innerHTML = "Thank you !";
     document.getElementById("inputMsg").style.color = "black";
   }
+
+  form.reset();
 });
 
 function validateEmail(email) {
   var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
-
-
-// document.getElementById("error-icon").classList.add("error");
